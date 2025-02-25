a5365320eda3448be8d3d4c310e1be27d5cea4e3
Muhammad Zeeshan Zahher <shan6818@gmail.com>
2025-02-25 12:10:57 +0500

    database changed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,42 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
   def index
+    @items=Item.all
+  end
+
+  def new
+    @item=Item.new
+  end
+
+  def create
+    @item=Item.new(item_params)
+    if @item.save
+      flash[:notice] = 'Item was successfully created.'
+      redirect_to items_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      flash[:notice] = 'Item was successfully updated.'
+      redirect_to items_path
+    else
+      render :edit ,status: :unprocessable_entity
+    end
+  end
+
+ 
+  private
+
+  def set_item
+    @item=Item.find(params[:id])
+  end
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
   end
 end
