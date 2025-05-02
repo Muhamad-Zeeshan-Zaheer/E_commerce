a1cdd87b67087478caafee1a95c0b3a5a5677e99
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-05-02 16:04:51 +0500

    changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,27 @@
 class ProductsController < ApplicationController
   def index
+    @products=Product.all
+  end
+  def new
+    @product=Product.new
+  end
+  def create
+    @product=Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+  def destroy
+    
   end
 end
