c3731de026452dbf98a120173277088c4ae4c2f8
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-06-05 12:25:25 +0500

    devise error resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 end
 def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name address phone])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name address phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone])
 end
